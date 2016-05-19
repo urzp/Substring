9301881fff9da8d346870bc9027417569a2dca4b
urzp <urzp002@mail.ru>
2016-05-19 20:16:03 +0300

    create main metod

--- Substring.rb
+++ Substring.rb
@@ -1,1 +1,12 @@
+#substrings('Howdy partner, sit down! How's it going?', dictionary)
+#=> {'down'=>1, 'how'=>2, 'howdy'=>1,'go'=>1, 'going'=>1, 'it'=>2, 'i'=> 3, 'own'=>1,'part'=>1,'partner'=>1,'sit'=>1}
+
+
+def substrings(text,dictionary)
+	
+end
+
+text="Howdy partner, sit down! How's it going?"
 dictionary = ['below','down','go','going','horn','how','howdy','it','i','low','own','part','partner','sit']
+
+substrings(text,dictionary)
